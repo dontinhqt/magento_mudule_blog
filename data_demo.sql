aa8d20c45c1bcaf911fb9190b77745bf852bf6cf
Tinh Nguyen <dontinhqt@gmail.com>
2017-06-09 09:55:07 +0700

    data demo

---
+++ data_demo.sql
@@ -0,0 +1,8 @@
+INSERT INTO `ashsmith_blog_post`
+    (`post_id`, `url_key`, `title`, `content`, `is_active`, `creation_time`, `update_time`)
+    VALUES
+    (NULL, 'test-1', 'test title 1', 'testing content 1', 1, '2015-08-17 22:59:00', '2015-07-17 22:59:00');
+INSERT INTO `ashsmith_blog_post`
+    (`post_id`, `url_key`, `title`, `content`, `is_active`, `creation_time`, `update_time`)
+    VALUES
+    (NULL, 'test-2', 'test title 2', 'testing content 2', 1, '2015-08-17 22:59:00', '2015-07-17 22:59:00');
